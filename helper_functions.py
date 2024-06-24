94ddafcdb1cb6feb76ffd89c01981630fbeabdc3
AbrahamH <75340597+Stonepaw90@users.noreply.github.com>
2024-06-24 11:39:38 -0400

    init commit

---
+++ helper_functions.py
@@ -0,0 +1,116 @@
+import random
+import string
+import streamlit as st
+import time
+
+def get_dictionary():
+  word_file = open("words_alpha.txt")
+  words = word_file.readlines()
+  word_file.close()
+  #no_duplicates = [i[:-1] for i in words if len(set(i[:-1])) == word_len & len(i[:-1]) == word_len]
+  no_duplicates = [i[:-1] for i in words]
+  return no_duplicates
+
+def blue_bold(text):
+    return f"<span style='font-size: 24px; color: #3A96DD;'><b>{text}</b></span>"
+
+def title_blue(text):
+    return f"<span style='font-size: 38px; color: #3A96DD;'><b>{text}</b></span>"
+
+
+
+def write_text(text, font_size = 24,
+               header_size = None,
+               alignment = "left",
+               text_color = "black",
+               font_family = "Source Sans Pro"):#"sans-serif"):
+    if header_size is None:
+        tag = "span"
+        size_text = f"font-size: {font_size}px; "
+    else:
+        tag = f"h{header_size}"
+        size_text = ""
+    st.markdown(f"<{tag} style='"
+                    f"{size_text}"
+                    f"text-align: {alignment}; "
+                    f"font-family: {font_family}; "
+                    f"color: {text_color};'"
+                f">{text}</{tag}>",
+                unsafe_allow_html=True)
+
+
+
+ALPHABET = set(string.ascii_lowercase)
+# Dictionary of Scrabble letter values
+scrabble_scores = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2,
+    'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1,
+    'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
+    'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
+}
+WORD_BANK = get_dictionary()
+
+
+class Tiles:
+    def __init__(self, tiles = None):
+        self.banned = random.choice(list(ALPHABET))
+        self.newA = ALPHABET.difference(self.banned)
+        if tiles is None:
+            self.Tiles = set(random.sample(list(self.newA), 11))
+        else:
+            self.Tiles = set(tiles)
+
+    def __add__(self, other):
+        #self.Tiles = self.Tiles.union(set(other))
+        return Tiles(self.Tiles.union(set(other)))
+
+    def __sub__(self, other):
+        #self.Tiles = self.Tiles.difference(set(other))
+        return Tiles(self.Tiles.difference(set(other)))
+
+    def draw(self, k):
+        legal_draw = self.newA.difference(self.Tiles)
+        return random.sample(list(legal_draw), k = k)
+
+    def exchange(self, old_letters):
+        new_letters = self.draw( k = len(set(old_letters)))
+        st.subheader("You draw the new letters:")
+        self.display(new_letters)
+        return (self - old_letters) + new_letters
+
+    def asList(self):
+        return sorted(self.Tiles)
+
+    def __str__(self):
+        return f"Tiles: {sorted(self.Tiles)}\n" \
+               f"Banned: {self.banned}\n"
+        #return sorted(self.Tiles)
+
+    def display_11(self):
+        self.display(letters = sorted(self.Tiles))
+
+    def display(self, letters):
+        cols = st.columns(11)
+        for i in range(min(11, len(letters))):
+            upper_letter = str.upper(letters[i])
+            if upper_letter in ["A", "E", "I", "O", "U"]:
+                upper_letter = blue_bold(upper_letter)
+            with cols[i]:
+                write_text(upper_letter)
+
+    def check_spell(self, spell):
+        return all([let in self.Tiles for let in spell])
+        #letter_good = [spell.count(i) <= self.Tiles.count(i) for i in self.Tiles]
+
+
+
+def init_session_state(dict_name, dict_val):
+    if dict_name not in st.session_state:
+        st.session_state[dict_name] = dict_val
+
+def initialize_whole_session_state():
+    init_session_state("timer", 60)
+    init_session_state("BANK", Tiles())
+    init_session_state("SCORE", 0)
+    init_session_state("spell", '')
+    init_session_state("spell_widget", '')
---
+++ main.py
@@ -0,0 +1,105 @@
+from helper_functions import *
+
+
+st.set_page_config(page_title = "Incantations",
+                   page_icon=":male_mage:",
+                   layout="wide",
+                   #initial_sidebar_state="collapsed",
+                   menu_items={"about":"Created by :mage: Abraham Holleran and the Nigros :male_mage: :female_mage:."})
+
+def calculate_scrabble_score(word):
+    #Calculate the Scrabble score for a given word.
+    score = 0
+    for letter in word.upper():
+        if letter in scrabble_scores:
+            score += scrabble_scores[letter]
+    return score
+
+def start_timer(duration):
+    #con = st.container()
+    while duration:
+        mins, secs = divmod(duration, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        st.toast(f"⏳ Time Remaining: {timer}")
+        time.sleep(1)
+        duration -= 1
+        #con.empty()
+
+
+def intro():
+    st.markdown(f"<span style = 'font_size: 24px;'><h1>:male_mage: Inc{title_blue('a')}nt{title_blue('a')}t{title_blue('io')}ns</h1></span>", unsafe_allow_html = True)
+
+    cols = st.columns([10,12])
+    with cols[1]:
+        write_text(
+          f"The banned letter is <span style='font-size: 60px; color: #3A96DD;'>{str.upper(st.session_state.BANK.banned)}</span> .",
+          header_size=1)
+    with cols[0]:
+        write_text(f"Your score is <span style='font-size: 60px; color: #3A96DD;'>{st.session_state.SCORE}</span>.", header_size=1)
+
+def submit():
+    st.session_state.spell = st.session_state.spell_widget
+    st.session_state.spell_widget = ''
+
+def cast_spell():
+    st.text_input(":sparkles: Cast your spell, or enter two letter to exchange them.", key = "spell_widget", on_change=submit, value='')
+    input_word = st.session_state.spell
+    if len(input_word) == 0:
+        pass
+    elif not st.session_state.BANK.check_spell(input_word):
+        st.write(f"The spell fizzles out :face_with_spiral_eyes:. You need to only use letters from your tile bank, {st.session_state.BANK.asList()}.")
+
+    elif len(input_word) == 1:
+        st.warning("Your spell must be at least three letters long.")
+
+    elif (len(input_word) == 2) and (len(set(input_word)) == 2): #"dad" and "aa" do not work but "ad" does
+        st.session_state.BANK = st.session_state.BANK.exchange(input_word)
+        st.session_state.spell = ''
+        time.sleep(2)
+
+        st.rerun()
+
+    elif input_word in WORD_BANK:
+        word_score = calculate_scrabble_score(input_word)
+        st.session_state.SCORE += word_score
+        emoji = random.choice(["comet", "sparkler", "firewords", "stars", "star2", "boom", "fire"])
+        st.markdown(
+            f":sparkles: You cast {blue_bold(input_word.upper() + '!')} :{emoji}: Your score increases by {word_score}.",
+            unsafe_allow_html=True)
+        st.session_state.BANK = st.session_state.BANK.exchange(input_word)
+        st.session_state.spell = ''
+        time.sleep(2)
+        st.rerun()
+
+    else:
+        st.warning("That's not a word.")
+
+    return input_word
+
+    #what_letters_lose = set(st.text_input(":heavy_exclamation_mark: Panic! Trade two letters for new ones."))
+
+def trade():
+    with st.form("Tradein2"):
+        what_letters_lose = st.multiselect(
+            ":arrows_clockwise: Select two letters to trade-in for new ones.",
+            st.session_state.BANK.asList(),
+            default=[],
+            max_selections=2
+        )
+        if st.form_submit_button("Submit"):
+            if len(set(what_letters_lose)) == 2:
+                st.session_state.BANK = st.session_state.BANK.exchange(what_letters_lose)
+            elif len(what_letters_lose) != 0:
+                st.warning("Select two tiles to trade in.")
+
+
+def main():
+    initialize_whole_session_state()
+
+    intro()
+    st.header("Your letters are:")
+    st.session_state.BANK.display_11()
+    input = cast_spell()
+
+if __name__ == '__main__':
+    main()
